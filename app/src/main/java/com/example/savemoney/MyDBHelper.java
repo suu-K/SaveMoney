fbc23071795a2b525f590351451a4784fe6d3b01
suuK <kswcks1457@gmail.com>
2021-12-03 02:46:00 +0900

    add expenses page

---
+++ app/src/main/java/com/example/savemoney/MyDBHelper.java
@@ -0,0 +1,53 @@
+package com.example.savemoney;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    public MyDBHelper(Context context) {
+        super(context, "SaveMoney", null, 1);
+    }
+
+    @Override
+    public void onCreate(@NonNull SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE card (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT NOT NULL," +
+                "kinds TEXT NOT NULL," +
+                "interestRate REAL" +
+                ");");
+        db.execSQL("CREATE TABLE expenses (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "amount INTEGER NOT NULL," +
+                "category TEXT NOT NULL," +
+                "content TEXT," +
+                "method TEXT NOT NULL," +
+                "cardId INTEGER REFERENCES card(id)," +
+                "installmentPeriod INTEGER," +
+                "interest," +
+                "monExpend INTEGER," +
+                "date DATE DEFAULT CURRENT_TIMESTAMP" +
+                ");");
+        db.execSQL("CREATE TABLE fixedExpenses (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT NOT NULL," +
+                "amount INTEGER NOT NULL" +
+                ");");
+        db.execSQL("CREATE TABLE category (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT NOT NULL" +
+                ");");
+    }
+
+    @Override
+    public void onUpgrade(@NonNull SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS card");
+        db.execSQL("DROP TABLE IF EXISTS expenses");
+        db.execSQL("DROP TABLE IF EXISTS fixedExpenses");
+        db.execSQL("DROP TABLE IF EXISTS category");
+        onCreate(db);
+    }
+}
